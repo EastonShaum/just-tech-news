12cc7e01468fa541323c0dd7430b01cbcc87bed0
Easton Shaum <Eastonshaum@gmail.com>
2021-07-26 14:32:02 -0600

    added the homepage with hondlebars

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controller');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+
+
+// start app either locally 3001 or the port assigned
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// import handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
@@ -16,3 +27,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
